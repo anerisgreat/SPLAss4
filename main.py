91ca99b8d0d3c05267c991c5d079acb8d913c83c
Aner Zakovitch <anerisgreat@gmail.com>
2021-01-15 16:59:00 +0200

    Added gitignore. Added summarys

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ def process_order_receive(p_name, p_amount, p_date):
     logistic.count_received += p_amount
     dist_repo.logistics.update(logistic)
     
+    #Getting new vaccine ID
     new_vaccine_id = dist_repo.vaccines.get_maxid() + 1
+
+    #Create new entry
     vaccine = Vaccine(new_vaccine_id, p_date, supplier.id, p_amount)
     dist_repo.vaccines.insert(vaccine)
 
@@ -24,28 +27,38 @@ def process_order_send(p_location, p_amount):
     clinic = dist_repo.clinics.find_by_location(p_location)
     logistic = dist_repo.logistics.find(clinic.logistic)
 
+    #Get generator to iterate over vaccines by date
     vaccine_gen = dist_repo.vaccines.get_all_by_date_generator()
 
+    #Keeping track of amount left
     amount_left = p_amount
+
     while(amount_left > 0):
+        #Getting next vaccine. If no more left we're done.
         try:
             vaccine = next(vaccine_gen)
         except StopIteration:
             break
 
+        #Taking maximum amount from vaccine
         amount_from_vaccine = min(vaccine.quantity, amount_left)
         amount_left -= amount_from_vaccine
         vaccine.quantity -= amount_from_vaccine
+
+        #Checking if vaccine now empty. If so, remove.
         if(vaccine.quantity == 0):
             dist_repo.vaccines.del_by_id(vaccine.id)
         else:
             dist_repo.vaccines.update(vaccine)
 
+    #Checking how many are left. If enough vaccines, should be full amount.
     amount_processed = p_amount - amount_left
     
+    #Update clinic
     clinic.demand -= amount_processed
     dist_repo.clinics.update(clinic)
 
+    #Update logistic
     logistic.count_sent += amount_processed
     dist_repo.logistics.update(logistic)
 
@@ -58,12 +71,15 @@ def get_values_for_summary():
 
 def load_config_file_to_db(p_config_fname):
     with open(p_config_fname, 'r') as rfile:
-        liter = iter(rfile.readlines())
+        #Creating generator for split lines
+        split_liter = iter(l.rstrip().split(',') for l in rfile.readlines())
 
-    nvaccines, nsuppliers, nclinics, nlogistics = map(int, next(liter).split(','))
+    #Get number of each type of entry from first line
+    nvaccines, nsuppliers, nclinics, nlogistics = map(int, next(split_liter))
 
-    for i in range(nvaccines):
-        sid, sdate, ssupplier, squantity = next(liter).split(',')
+    #Load vaccines into database
+    for _ in range(nvaccines):
+        sid, sdate, ssupplier, squantity = next(split_liter)
         dist_repo.vaccines.insert(
             Vaccine(
                 int(sid),
@@ -72,8 +88,9 @@ def load_config_file_to_db(p_config_fname):
                 int(squantity)
             ))
 
-    for i in range(nsuppliers):
-        sid, sname, slogistic = next(liter).split(',')
+    #Load suppliers into database
+    for _ in range(nsuppliers):
+        sid, sname, slogistic = next(split_liter)
         dist_repo.suppliers.insert(
             Supplier(
                 int(sid),
@@ -81,8 +98,9 @@ def load_config_file_to_db(p_config_fname):
                 int(slogistic)
             ))
 
-    for i in range(nclinics):
-        sid, slocation, sdemand, slogistic = next(liter).split(',')
+    #Load clinics into database
+    for _ in range(nclinics):
+        sid, slocation, sdemand, slogistic = next(split_liter)
         dist_repo.clinics.insert(
             Clinic(
                 int(sid),
@@ -91,8 +109,9 @@ def load_config_file_to_db(p_config_fname):
                 int(slogistic)
             ))
     
-    for i in range(nlogistics):
-        sid, sname, scount_sent, scount_received = next(liter).split(',')
+    #Load logistics into database
+    for _ in range(nlogistics):
+        sid, sname, scount_sent, scount_received = next(split_liter)
         dist_repo.logistics.insert(
             Logistic(
                 int(sid),
@@ -107,15 +126,20 @@ def execute_orders_with_summary(p_order_fname, p_summary_fname):
 
     with open(p_summary_fname, 'w') as ofile:
         for splitl in split_liter:
+            #Check which type of order
             if(len(splitl) == 2 or len(splitl) == 3):
+                #Send order
                 if len(splitl) == 2:
                     process_order_send(splitl[0], int(splitl[1]))
+                #Receive order
                 else:
                     process_order_receive(splitl[0],
                                     int(splitl[1]),
                                     dt.strptime(splitl[2], DT_FORMAT_STR))
-            summary = get_values_for_summary()
-            ofile.write(f'{summary[0]},{summary[1]},{summary[2]},{summary[3]}\n')
+
+                #Write to summary
+                summary = get_values_for_summary()
+                ofile.write(f'{summary[0]},{summary[1]},{summary[2]},{summary[3]}\n')
 
 def main(args):
     load_config_file_to_db(args[0])
